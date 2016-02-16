ab6f26448f1b098e712c03b98aefd5a4d784c17e
MundoGister <mundogister@gmail.com>
2016-02-16 19:14:42 +0100

    Ejercicio 2

---
+++ js2/index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/*
+  Crear un bucle for que imprima los mensajes:
+  Valor de i: 0
+  Valor de i: 2
+  ... hasta ...
+  Valor de i: 9
+*/
+var i;
+for (i=0; i<10; i++) {
+    alert(i)
+}
+
+
+/*
+  Crear un bucle for que imprima los mensajes: con números pares
+  Valor de i: 0
+  Valor de i: 2
+  Valor de i: 4
+  ... hasta ...
+  Valor de i: 8
+*/
+for (i=0; i<9; i=i+2) {
+    alert(i)
+}
+
+/*
+  Crear un bucle dentro de otro (anidados) que imprima
+  Valor de i: 0
+  Valor de j: 0
+  
+  Valor de i: 0
+  Valor de j: 1
+  
+  Valor de i: 0
+  Valor de j: 2
+  
+  ... hasta ...
+  
+  Valor de i: 9
+  Valor de j: 9
+*/
+var i,j;
+for (i=0; i<10; i++) {
+    for (j=0; j<10; j++) {
+        document.writeln(i)
+        document.writeln(j)
+        document.writeln(',')
+    }
+}
