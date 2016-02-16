1a7d42670847121fd67dc708a1c7027423637499
MundoGister <mundogister@gmail.com>
2016-02-16 18:21:00 +0100

    Primeros ejercicios
    Ejercicios

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// Almacenar en una variable el resultado de sumar 1 y 2
+var a = 1 + 2;
+console.log(a);
+
+// alert(a); // Esto muestra una ventana emergente
+alert(a);
+
+// Almacenar en una variable el resultado de dividir 6 entre 2
+var b = 6/2;
+
+// Almacenar en una variable el precio de un artículo de 20€ aplicándole el 21% de IVA .
+var precio_iva = 20 + (20*0.21);
+
+// Definir una variable con el valor 4 y utilizar el operador (```++```) para incrementar en uno su valor.
+var valor = 4;
+console.log(valor);
+
+// Definir una variable que almacene la concatenación de dos cadenas de texto.
+ var texto_1 = 'Hola'
+ var texto_2 = 'K ase'
+ console.log(texto_1 + ' ' + texto_2);
+ 
+// Definir una variable ```price``` con el valor ```19.99```y aplicar la abreviación ```/=``` para dividirlo entre ```1.21``` para obtener el precio sin IVA.
+var price = 19.99, valor_2 = 1.21, precio_sin_iva;
+precio_sin_iva = price / valor_2;
+
+// Asignar a dos variables valores booleanos y hacer al menos una operación combinando un operador lógico: **AND** (```&&```) o **OR** (```||```)
+var primera = true;
+var segunda = false;
+console.log(primera && segunda);
+
+// Realizar 4 expresiones que utilicen operadores relacionales (```<```, ```==```, ```!=``` y ```===```) y almacenen los valores en tres variables distintas.
+var exp = 30 < 2;
+var exp_2 = 15 == 15;
+var exp_3 = 15 != 15;
+var exp_4 = 15 === 15;
+console.log(exp);
+console.log(exp_2);
+console.log(exp_3);
+console.log(exp_4);
+
