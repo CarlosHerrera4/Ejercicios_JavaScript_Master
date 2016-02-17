558b8016addf6b2b88c681c34720c1b3abb4b732
MundoGister <mundogister@gmail.com>
2016-02-17 16:41:09 +0100

    Ejercicio 3

---
+++ js3/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/* 
+  Declara un objeto mapa que tenga como información:
+    - Una propiedad basemap con valor "topo" como string
+    - Una propiedad center con un array de dos valores (-3.688510, 40.453010)
+    - Una propiedad zoom con valor 16
+*/
+var mapa = {
+    'basemap' : 'topo',
+    'center' : [-3.688510, 40.453010],
+    'zoom' : 16
+};
+
+/*
+  Añádele una propiedad 'autoResize' a verdadero (booleano)
+*/
+mapa.autoResize = true;
+
+
+/*
+  Imprime todos los valores del objeto usando un blucle for
+*/
+for (var property in mapa) {
+    if (mapa.hasOwnProperty(property)) {
+        document.writeln(mapa[property])
+    }
+}
